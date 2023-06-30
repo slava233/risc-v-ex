e64b36678dfeff69a7d80ccc0bf5802999629556
Kirill Krinkin <kirill@krinkin.com>
2023-06-30 15:00:12 +0000

    fix wrong .data section

--- sine/sine.s
+++ sine/sine.s
@@ -2,8 +2,12 @@
 
 default_answer = 0x312d
 
-.text
+.section .data
 # if you need some data, put it here
+var:
+.align 8
+.space 100
+
 
 .section .text
 
@@ -13,10 +17,12 @@ default_answer = 0x312d
 #	a2 -- output string buffer for the string result
 sine:
 	# implement here
+
+	la	a7, var
 	
 	li	a3, default_answer
 	sw	a3, 0(a2)
-
+	sw	a3, 0(a7)
 
 	ret
 	
