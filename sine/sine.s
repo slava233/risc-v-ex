ed893241b6c323d388401ae1a7be5cf9e8c82702
slava233 <harik10346@gmail.com>
2023-08-05 19:36:32 +0300

    Removed 'one' variable from data section and integrated copy_one function in the end of sin function

--- sine/sine.s
+++ sine/sine.s
@@ -6,33 +6,19 @@ var:
 .align 8
 .space 100
 
-one:
-.asciz "1"
-
 .section .text
 
 sine:
-    lb a3, (a1)
-    lb t1, (one)
-    bne a3, t1, copy_input
-    beq a3, t1, copy_one
+    lb t1, (a1)
+    li t2, 49
+    bne t1, t2, copy_input
+    sb t2, (a2)
     ret
 
 copy_input:
-    input_loop:
-        lb t2, 0(a1)
-        sb t2, 0(a2)
-        addi a1, a1, 1
-        addi a2, a2, 1
-        bnez t2, input_loop
-        ret
-
-copy_one:
-	la a1, one
-    one_loop:
-        lb t0, 0(a1)
-        sb t0, 0(a2)
-        addi a1, a1, 1
-        addi a2, a2, 1
-        bnez t0, one_loop
-        ret
+    lb t0, (a1)
+    sb t0, (a2)
+    addi a1, a1, 1
+    addi a2, a2, 1
+    bnez t0, copy_input
+    ret
